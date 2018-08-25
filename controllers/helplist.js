fcbbb35ed62fb5c2b3aa5788ed665e81cb16567a
Chance Murray <chance.s.murray@gmail.com>
2018-08-24 21:25:57 -0600

    Sockets working on AddHelp!!!

--- controllers/helplist.js
+++ controllers/helplist.js
@@ -1,22 +1,54 @@
-const Sequelize = require('sequelize')
-let db = new Sequelize('helpsite', null, null, {
-    dialect: "sqlite",
-    storage: './db/helpsite.db',
-})
-exports.addHelp = (req, res) => {
 
-}
-exports.getHelpList = (req,res) => {
-    return db.query('SELECT * FROM help INNER JOIN users ON users.id=help.userId',{type:db.QueryTypes.SELECT})
+let getHelpList = (db) => {
+    return db.query('SELECT help.id,room,problem,firstName,lastName,displayName,netid FROM help INNER JOIN users ON users.id=help.userId',{type:db.QueryTypes.SELECT})
     .then(data => {
         //console.log('HELPLIST',data)
         return data
     })
 }
-exports.getPassList = (req,res) => {
-    return db.query('SELECT * FROM passoff INNER JOIN users ON users.id=passoff.userId',{type:db.QueryTypes.SELECT})
+let getPassList = (db) => {
+    return db.query('SELECT passoff.id,room,problem,firstName,lastName,displayName,netid FROM passoff INNER JOIN users ON users.id=passoff.userId',{type:db.QueryTypes.SELECT})
     .then(data => {
         //console.log('PassList',data)
         return data
+    })
+}
+exports.getBothLists = db => {
+    return Promise.all([getHelpList(db),getPassList(db)])
+}
+exports.selectUser = (db, username, password) => {
+    return db.query('SELECT id,firstName,lastName,displayName,netid,admin from users where netid=:netid AND password=:password',
+    {
+        type:db.QueryTypes.SELECT,
+        replacements: {
+            netid: username,
+            password
+        }
+    })
+}
+exports.selectUserById = (db, id) => {
+    return db.query('SELECT id,firstName,lastName,displayName,netid,admin from users where id=:id',
+    {
+        type:db.QueryTypes.SELECT,
+        replacements:{id}
+    })
+}         
+exports.addHelp = (req, db) => {
+    return db.query('INSERT INTO help (userId,room,problem) VALUES (:userid,:room,:problem);',
+    {
+        type:db.QueryTypes.INSERT,
+        replacements: {
+            userid: +req.user.id,
+            room: +req.query.room,
+            problem: req.query.problem
+        }
+    })
+    .then(data => {
+        //console.log(data)
+        return "Success"
+    })
+    .catch(e=> {
+        //console.log(e)
+        return "Error"
     })
 }
--- index.js
+++ index.js
@@ -1,51 +1,133 @@
 'use strict'
 const express = require('express')
-var session = require('express-session')
+let session = require('express-session')
+// let SequelizeStore = require('connect-session-sequelize')(session.Store);
+// let cookieParser = require('cookie-parser')
 const http = require('http')
 let app = express()
 let server = http.Server(app)
-let io = require('socket.io')(server);
+let passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+let io = require('socket.io')(server)
 let bp = require('body-parser')
-const helplist = require('./controllers/helplist')
-app.use(session({
-    secret: 'byu-it210-help-list-secret',
-    resave: false,
-    saveUninitialized: true
-}))
-app.use(bp.urlencoded({extended:false}))
 
+//Database Connection
+const Sequelize = require('sequelize')
+let db = new Sequelize('helpsite', null, null, {
+    dialect: "sqlite",
+    storage: './db/helpsite.db',
+    logging:false
+})
+
+//Helper Functions
+const helplist = require('./controllers/helplist')
+
+//EXPRESS SETTINGS
 app.set('views', 'views')
 app.set('view engine', 'ejs')
+app.use(session({
+    secret: 'byu-it210-help-list-secret',
+    
+    resave: false,
+    saveUninitialized: true,
+    
+}))
+//app.use(cookieParser('byu-it210-help-list-secret'))
+app.use(bp.urlencoded({extended:false}))
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    Promise.all([helplist.getHelpList(),helplist.getPassList()])
+// AUTHENTICATION STUFF
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    helplist.selectUser(db,username,password)
     .then(data => {
-        // console.log(data)
+        let user = data[0]
+        if(!user) return done(null,false,"Invalid username or password")
+        return done(null,user)
+    })
+    .catch(e=> {
+        console.log(e)
+        return done(e)
+    })
+  }
+))
+passport.serializeUser(function(user, cb) {
+    console.log(user)
+    cb(null, user.id);
+})
+passport.deserializeUser(function(id, cb) {
+    helplist.selectUserById(db,id)
+    .then(data => {
+        let user = data[0]
+        cb(null,user)
+    })
+});
+
+//Protect Middleware
+let Protect = (req,res,next) => {
+    if (req.user) next()
+    else res.redirect('/login')
+}
+//returns the user or 1
+let getUser = req => req.user ? req.user : 1
+
+// AUTHENTICATION View ROUTES
+app.get('/login', (req,res)=> {
+    res.render('login', {user:getUser(req)})
+})
+app.get('/register', (req,res)=> {
+    res.render('register', {user:getUser(req)})
+})
+
+// AUTHENTICATION Action ROUTES
+app.post('/auth/login',
+    passport.authenticate('local', 
+    { 
+        successRedirect: '/',
+        failureRedirect: '/login',
+        failureFlash: true 
+    }
+));
+app.post('/auth/register', (req,res) => {
+    res.send('ahhhh')
+})
+app.get('/logout',(req,res) => {
+    req.logout()
+    res.redirect('/login')
+})
+
+// APPLICATION ROUTES
+app.get('/',Protect, (req, res) => {
+    helplist.getBothLists(db)
+    .then(data => {
         
-        res.render('index',{user:1,help:data[0], pass:data[1]})
+        //console.log("USER",req.user)
+        res.render('index',{user:getUser(req),help:data[0], pass:data[1]})
     })
     .catch(e=>{res.status(500).send(`There was an error:${e}`)})
 })
 
-app.get('/login', (req,res)=> {
-    res.render('login')
-})
-app.post('/auth/login', (req,res) => {
-    console.log(req.body);
-    req.user=req.body
-    res.redirect('/')
-})
-
-app.get('/register', (req,res)=> {
-
-    res.render('register')
-})
-app.post('/auth/register', (req,res) => {
-    res.send('ahhhh')
-})
-app.get('/addHelp',(req,res)=>{
+app.get('/addHelp',Protect, (req,res)=>{
+    helplist.addHelp(req,db)
+    .then((status) => {
+        if(status="Success"){
+            helplist.getBothLists(db)
+            .then(data => {
+                io.sockets.emit('newlists',
+                {
+                    help:data[0],
+                    pass:data[1]
+                })
+            })
+        }
+    })
+    .catch(e => {
+        console.log(e)
+    })
     io.sockets.emit('asdf', {msg: 'IT WORKED!!!'}) //WORKS!!!
+    res.send('Socket Emitted')
 
 })
 app.post('/addPass',(req,res)=>{
@@ -57,7 +139,7 @@ io.on('connection', socket=> {
     socket.on('disconnect', function(){
         console.log('user disconnected');
     });
-    socket.on('test', ({msg}) => {
+    socket.on('test', (msg) => {
         console.log(msg)
     })
 
